5480e57fa3e48765df7342183aa7e3a0cbb30dbe
chendongsheng <sexluna@outlook.com>
2024-06-16 12:36:18 +0800

    feat: 体验一下MLIR 的OP结构
    Signed-off-by: chendongsheng <sexluna@outlook.com>

--- main.cpp
+++ main.cpp
@@ -9,7 +9,16 @@
 
 #include <iostream>
 
+#include "mlir/IR/AsmState.h"
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/Parser/Parser.h"
+#include "mlir/Support/FileUtilities.h"
 
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "llvm/Support/raw_ostream.h"
 using namespace std;
 using namespace mlir;
 
@@ -23,10 +32,38 @@ int main(int argc, char ** argv) {
     auto src = parseSourceFile<ModuleOp>(argv[1], &ctx);
 
     // 3. 简单输出 dialect
+    cout << "从文件中读取mlir" << endl;
     src->print(llvm::outs());
 
-    cout << "-----------------> " << endl;
-    src->dump();
+
+
+    cout << "使用代码,生成mlir" << endl;
+    // 4. create OpBuilder
+    OpBuilder builder(&ctx);
+    auto mod = builder.create<ModuleOp>(builder.getUnknownLoc());
+
+    // 5. 设置插入点
+    builder.setInsertionPointToEnd(mod.getBody());
+
+    // 6. 创建func
+    auto i32 = builder.getI32Type();
+    auto funcType = builder.getFunctionType({i32, i32}, i32);
+    auto func = builder.create<func::FuncOp>(builder.getUnknownLoc(), "funName", funcType);
+
+    // 7. 添加基本块
+    auto entry = func.addEntryBlock();
+    auto args = entry->getArguments();
+
+    // 8. 设置插入点
+    builder.setInsertionPointToEnd(entry);
+
+    // 9. 创建arith.addi
+    auto addi = builder.create<arith::AddIOp>(builder.getUnknownLoc(), args[0], args[1]);
+
+    // 10. 创建func.return
+    builder.create<func::ReturnOp>(builder.getUnknownLoc(), ValueRange({addi}));
+    mod.dump();
+
 
 
 
