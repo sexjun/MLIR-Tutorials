3477df8bafcd85fba41d4aa66ff267715af7520f
dsheng.chen <1196586501@qq.com>
2024-12-01 22:48:04 +0800

    feat: cmake add function add_all_subdirs()
    cmake add new funcation to add all sub_dirs
    
    Signed-off-by: dsheng.chen <1196586501@qq.com>

---
+++ cmake/add_all_subdirs.cmake
@@ -0,0 +1,13 @@
+macro(add_all_subdirs)
+  FILE(GLOB _CHILDREN RELATIVE ${CMAKE_CURRENT_SOURCE_DIR} ${CMAKE_CURRENT_SOURCE_DIR}/*)
+  SET(_DIRLIST "")
+  foreach(_CHILD ${_CHILDREN})
+    if(IS_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/${_CHILD} AND EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${_CHILD}/CMakeLists.txt)
+      LIST(APPEND _DIRLIST ${_CHILD})
+    endif()
+  endforeach()
+
+  foreach(subdir ${_DIRLIST})
+    add_subdirectory(${subdir})
+  endforeach()
+endmacro()
